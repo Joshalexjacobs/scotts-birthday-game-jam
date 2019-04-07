03c3d8e77cbb868e73c05dab400f1c21cde8b4ef
joshalexjacobs <joshalexjacobs@gmail.com>
2019-04-07 14:09:46 -0400

    some updates to zombies

--- Assets/Scripts/Enemies/Zombie.cs
+++ Assets/Scripts/Enemies/Zombie.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Zombie : Enemy {
 
@@ -11,15 +12,25 @@ public class Zombie : Enemy {
     public bool chasingTower = true;
     public float attackTimerMax = 5f;
 
+    public GameObject bite;
+
     private float attackTimer = 0f;
     private Tower tower;
+    private Player player;
+
+    private GameObject camera;
+    private ScreenShake screenShake;
 
     private SpriteRenderer spriteRenderer;
 
     void Start () {
         tower = FindObjectOfType<Tower>();
+        player = FindObjectOfType<Player>();
 
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        screenShake = camera.GetComponent<ScreenShake>();
 
         attackTimer = attackTimerMax;
     }
@@ -27,33 +38,56 @@ public class Zombie : Enemy {
     void Update() {
         if (!isDead) {
             if (chasingTower) {
-                MoveTowardsTower();
-                // LookForPlayer();
+                MoveTowardsObject(tower.transform, true);
+                LookForPlayer();
+            } else {
+                MoveTowardsObject(player.transform, true);
             }
-
         }
     }
 
-    private void MoveTowardsTower() {
+    private void MoveTowardsObject(Transform target, bool movingTowardsTower) {
         float step = speed * Time.deltaTime;
 
-        if (Vector3.Distance(transform.position, tower.transform.position) > 0.25f) {
-            transform.position = Vector3.MoveTowards(transform.position, tower.transform.position, step);
+        if (Vector3.Distance(transform.position, target.position) > 0.25f) {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         } else if (attackTimer >= attackTimerMax) {
-            tower.Damage(1f);
-            attackTimer = 0f;
+            AttackTower();
         }
 
-        if (attackTimer < attackTimerMax) {
+        if (movingTowardsTower && attackTimer < attackTimerMax) {
             attackTimer += Time.deltaTime;
         }
 
-        HandleZombieFlip(tower.transform);
+        HandleZombieFlip(target);
+    }
+
+    private void AttackTower() {
+        Instantiate(bite, tower.transform.position, Quaternion.identity);
+        tower.Damage(1f);
+        attackTimer = 0f;
+        screenShake.TriggerShake(0.05f);
     }
 
     private void LookForPlayer() {
-        // if player is within radius and the tower isnt,
-        // move towards the player
+        bool towerIsClose = false;
+        bool playerIsClose = false;
+
+        List<Collider2D> colliders = new List<Collider2D> (Physics2D.OverlapCircleAll(transform.position, 1f));
+
+        colliders.ForEach(collider => {
+            if (collider.transform.tag == "Player") {
+                playerIsClose = true;
+            }
+
+            if (collider.tag == "Tower") {
+                towerIsClose = true;
+            }
+        });
+
+        if(!towerIsClose && playerIsClose) {
+            chasingTower = false;
+        }
     }
 
     private void HandleZombieFlip(Transform target) {
